f9816f65fa6c5c88318b7b42f663e5217515fb89
KanekiEzz <ilyassezzam1337@gmail.com>
2025-08-07 10:15:05 +0100

    Create a DataFrame from List

---
+++ Create a DataFrame from List.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from typing import List
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    return pd.DataFrame(student_data, columns=['student_id', 'age'])
+
+data = [
+  [1, 15],
+  [2, 11],
+  [3, 11],
+  [4, 20]
+]
+
+df = createDataframe(data);
+print(df)
+print("Indexes: ", df.index.tolist())
+print("Indexes: ", df.age.tolist())
+print("Indexes: ", df.student_id.tolist())
